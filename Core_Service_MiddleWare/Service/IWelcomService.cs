edbc18bb88cfe166e6bb4eaed63e7c033b449e2e
lxg <1436215718@qq.com>
2019-04-07 20:25:28 +0800

    添加项目文件。

---
+++ Core_Service_MiddleWare/Service/IWelcomService.cs
@@ -0,0 +1,14 @@
+﻿namespace Core_Service_MiddleWare.Service
+{
+    public interface IWelcomService
+    {
+        string WelcomeMessage();
+    }
+    public class WelcomService : IWelcomService
+    {
+        public string WelcomeMessage()
+        {
+            return "Hello!!!     __From WelcomService";
+        }
+    }
+}
---
+++ Core_Service_MiddleWare/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using Core_Service_MiddleWare.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Core_Service_MiddleWare
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //服务注册
+            services.AddSingleton<IWelcomService, WelcomService>();
+        }
+        
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            IWelcomService welcomService,
+            ILogger<Startup> logger) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            //中间件
+            app.Use(next =>
+            {
+                logger.LogInformation("Use中间件...");
+                return async httpContext =>
+                {
+                    logger.LogInformation("中间件里判断...");
+                    if (httpContext.Request.Path.StartsWithSegments("/test"))
+                        await httpContext.Response.WriteAsync("TestMiddleWare");
+                    else
+                        await next(httpContext);
+                    
+                };
+            });
+            app.UseWelcomePage(new WelcomePageOptions { Path = "/welcome" });
+            app.UseFileServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(welcomService.WelcomeMessage());
+            });
+        }
+    }
+}
