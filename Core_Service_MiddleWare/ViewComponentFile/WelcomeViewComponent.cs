70a605197886660573dea8699fb0a3aa09be0cd5
lxg <1436215718@qq.com>
2019-04-18 23:43:49 +0800

    ViewComponent

---
+++ Core_Service_MiddleWare/ViewComponentFile/WelcomeViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core_Service_MiddleWare.Models;
+using Core_Service_MiddleWare.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Service_MiddleWare.ViewComponentFile
+{
+    public class WelcomeViewComponent:ViewComponent
+    {
+        private readonly IRepository<Student> _repository;
+        public WelcomeViewComponent(IRepository<Student> repository)
+        {
+            _repository = repository;
+        }
+        public IViewComponentResult Invoke(int a)
+        {
+            var count = _repository.GetAll().Count().ToString();
+            return View("Default", count);
+        }
+    }
+}
